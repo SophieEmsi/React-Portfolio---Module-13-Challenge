872f22c041c78058a588b44d66106e493b88631b
Sophie McNally <soph_mcnally@msn.com>
2023-05-20 17:55:14 +0100

    adding navbar and styling page

--- Starter/my-app/src/App.js
+++ Starter/my-app/src/App.js
@@ -1,57 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Navbar from './navbar';
-import Home from './home';
-import About from './About';
-import Experience from './Experience';
-import Portfolio from './Portfolio';
-import Contact from './Contact';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Navbar from './Components/Pages/navbar'; 
+import Home from './Components/Pages/home';
+import About from './Components/Pages/about';
+import Experience from './Components/Pages/experience';
+import Portfolio from './Components/Pages/portfolio';
+import Contact from './Components/Pages/contact';
 
 const App = () => {
   return (
     <Router>
-    <Navbar />
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/experience" component={Experience} />
-      <Route path="/portfolio" component={Portfolio} />
-      <Route path="/contact" component={Contact} />
-    </Switch>
-  </Router>
-);
-};
-    <div style={{ display: 'flex', height: '100vh' }}>
-      <div
-        style={{
-          flex: 1,
-          backgroundColor: '#f2f2f2',
-          overflow: 'hidden',
-        }}
-      >
-       
-        <header style={{ border: '1px solid black', padding: '50px', marginTop: '80px' }}>
-          
-          <h1>Sophie McNally</h1>
-          <h2>Front-End Web Developer</h2>
-          <p>I am a newly graduated bootcamp student looking for a junior or internship role</p>
-        </header>
-      </div>
-      <div
-        style={{
-          flex: 1,
-          backgroundColor: '#fff',
-          overflowY: 'scroll',
-        }}
-      >
-        <div style={{ padding: '15px' }}>
-          {/* Content for the scrollable column (right side) */}
+      <Navbar />
+      <div style={{ display: 'flex', height: '100vh' }}>
+        <div
+          style={{
+            flex: 1,
+            backgroundColor: '#f2f2f2',
+            overflow: 'hidden',
+          }}
+        >
+          {/* Content for the static column (left side) */}
+          <header style={{ border: '1px solid black', padding: '50px', marginTop: '80px' }}>
+            <h1>Sophie McNally</h1>
+            <h2>Front-End Web Developer</h2>
+            <p>I am a newly graduated bootcamp student looking for a junior or internship role</p>
+          </header>
+          <Nav defaultActiveKey="/" className="flex-column">
+            <Nav.Link as={Link} to="/" eventKey="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/about" eventKey="/about">About</Nav.Link>
+            <Nav.Link as={Link} to="/experience" eventKey="/experience">Experience</Nav.Link>
+            <Nav.Link as={Link} to="/portfolio" eventKey="/portfolio">Portfolio</Nav.Link>
+          </Nav>
+        </div>
+        <div
+          style={{
+            flex: 1,
+            backgroundColor: '#fff',
+            overflowY: 'scroll',
+          }}
+        >
+          <div style={{ padding: '15px' }}>
+            {/* Content for the scrollable column (right side) */}
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/experience" element={<Experience />} />
+              <Route path="/portfolio" element={<Portfolio />} />
+              <Route path="/contact" element={<Contact />} />
+            </Routes>
+          </div>
         </div>
       </div>
-    </div>
-
-
-
-export default App;
+    </Router>
+  );
+};
